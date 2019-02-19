da28ad44ca0a00806c4f58978fdbc3b5a38509fe
Florian Aymanns <florian.aymanns@epfl.ch>
2019-02-19 10:49:27 +0100

    increased times for confocal images

--- commands/makeAverageBrain-sync.sh
+++ commands/makeAverageBrain-sync.sh
@@ -86,7 +86,7 @@ while [ $i -le $NOITERATIONS ]; do
 	# which waits until (all?) array jobs are finished
 	CURREFBRAIN=${REFBRAIN}${i}
 	# passed REFBRAIN REGROOT REGBINDIR and MUNGERDIR variables
-	sbatch --mail-user=aymanns --mail-type=FAIL --array=1-${NOIMAGES} --wait --nodes 1 --ntasks 1 --cpus-per-task 28 --time 1:30:00 --mem 128G --partition parallel --workdir "$REGROOT"/jobs "$REGROOT"/commands/warpcmdIteration.sh ${CURREFBRAIN} ${REGROOT} ${REGBINDIR} ${MUNGERDIR}
+	sbatch --mail-user=aymanns --mail-type=FAIL --array=1-${NOIMAGES} --wait --nodes 1 --ntasks 1 --cpus-per-task 28 --time 2:30:00 --mem 128G --partition parallel --workdir "$REGROOT"/jobs "$REGROOT"/commands/warpcmdIteration.sh ${CURREFBRAIN} ${REGROOT} ${REGBINDIR} ${MUNGERDIR}
 	#sbatch --mail-user=aymanns --mail-type=FAIL --wait --nodes 1 --ntasks 1 --cpus-per-task 28 --time 1:00:00 --mem 128G --partition debug --workdir "$REGROOT"/jobs "$REGROOT"/commands/warpcmdIteration.sh ${CURREFBRAIN} ${REGROOT} ${REGBINDIR} ${MUNGERDIR}
 	
 	# add one to $i and therefore the number of the refbrain e.g. IS2-1 goes to IS2-2
@@ -95,7 +95,7 @@ while [ $i -le $NOITERATIONS ]; do
 
 	if [ ! -f "${REGROOT}/refbrain/${NEWREFBRAIN}.nrrd" ] ; then
 		# if average brain doesn't exist (eg if a run was interrupted then make it)
-		sbatch --mail-user=aymanns --mail-type=FAIL --wait --nodes 1 --ntasks 1 --cpus-per-task 28 --time 1:00:00 --mem 128G --partition debug --workdir "$REGROOT/jobs" "$REGROOT/commands/avgcmdIterationPadOut.sh" ${CURREFBRAIN} ${NEWREFBRAIN} ${REGROOT} ${REGBINDIR}
+		sbatch --mail-user=aymanns --mail-type=FAIL --wait --nodes 1 --ntasks 1 --cpus-per-task 28 --time 2:30:00 --mem 128G --partition parallel --workdir "$REGROOT/jobs" "$REGROOT/commands/avgcmdIterationPadOut.sh" ${CURREFBRAIN} ${NEWREFBRAIN} ${REGROOT} ${REGBINDIR}
 	fi
 	if [ ! -f "${REGROOT}/refbrain/${NEWREFBRAIN}.nrrd" ] ; then
 		echo Exiting $PROGNAME since avg_adm failed to make $NEWREFBRAIN
